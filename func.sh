b1d2f4245126663ea909416651d99bb122d1f9f7
Taylor Monacelli <taylor.monacelli@streambox.com>
2016-09-11 12:37:37 -0700

    yada-yada

--- func.sh
+++ func.sh
@@ -1,14 +1,18 @@
-PATH="/c/Program Files/Oracle/Virtualbox":$PATH
-
 function vmsnap()
 {
 	vm="$1"
+
+	PATH="/c/Program Files/Oracle/Virtualbox":$PATH
+
 	VBoxManage snapshot "$vm" take "bootstrapped" --description "bootstrapped so we save time"
 }
 
 function restorecurrent()
 {
 	vm="$1"
+
+	PATH="/c/Program Files/Oracle/Virtualbox":$PATH
+
 	VBoxManage controlvm $vm poweroff
 	sleep 1
 	VBoxManage snapshot $vm restorecurrent
